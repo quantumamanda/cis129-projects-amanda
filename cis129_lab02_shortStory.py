504182a770adfcfd202f52da3d58db3629b9267e
quantumamanda <awithrow@mail.pima.edu>
2025-01-30 23:05:07 -0700

    Update cis129_lab02_shortStory.py with a new creative short story for my first lab.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print ('Hello, World!')
+#Author: Amanda Withrow
+#My first Python lab in my CIS129 course!
+#This prints a simply story.
+print('I wanted to become a wizard, so I asked around who I could study under.')
+print('I was told to go to the Academy of Raya Lucaria, so I began on my journey to the Academy.')
+print('When I got to the Academy, I met an instructor there who was a wizard, and asked them to become my teacher.')
+print('It turned out it wasn't actual spells that I'd be learning, and instead they handed me a computer and Python textbook and told me I'd become a wizard - a programming wizard!')
